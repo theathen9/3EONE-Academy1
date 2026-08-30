0cdf5c2851b5ed949722144c5ae32421e9ea2e3a
theathen9 <theathen36@gmail.com>
2026-08-30 18:02:06 +0700

    Add debug URL route for application diagnostics

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/debug-url', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'url' => url('/'),
+        'asset' => asset('build/assets/app.css'),
+        'scheme' => $request->getScheme(),
+        'is_secure' => $request->isSecure(),
+        'forwarded_proto' => $request->header('x-forwarded-proto'),
+        'app_url' => config('app.url'),
+        'asset_url' => config('app.asset_url'),
+    ]);
+});
