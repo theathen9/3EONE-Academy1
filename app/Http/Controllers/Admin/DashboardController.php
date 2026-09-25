5671c4ae1b3059ed502cb0372cf4bcf9fb225b15
theathen9 <theathen36@gmail.com>
2026-09-25 21:55:10 +0700

    Modify index method to return a string message

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     public function index()
-    {
-        return view('admin.dashboard');
-    }
+{
+    return 'Admin dashboard controller works';
+}
 }
